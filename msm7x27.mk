85f9dedd1eb6104539a04eaf8f9053963bac782b
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2014-04-04 22:19:59 -0600

    Kitkat not use bugmailer

--- msm7x27.mk
+++ msm7x27.mk
@@ -77,11 +77,6 @@ PRODUCT_PACKAGES += \
     FmRxService \
     Fmapplication
 
-# Bugmailer
-PRODUCT_PACKAGES += send_bug
-PRODUCT_COPY_FILES += \
-        system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
-        system/extras/bugmailer/send_bug:system/bin/send_bug
 
 # Permissions
 PRODUCT_COPY_FILES += \
